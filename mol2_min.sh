2eef606224c1798e44e35b727428b1b1fa331bf6
Leticia Figueira <jlcfigueira@gmail.com>
2024-02-03 11:47:14 -0300

    ADiciona o pdbqt e a configuração do Autodock Vina

--- mol2_min.sh
+++ mol2_min.sh
@@ -5,6 +5,6 @@ mkdir $pasta
 
 for n in $pastaOrigem/*.mol2
 do
-arquivo=$(basename -- $n)
-obabel -imol -omol --minimize --log --crit 1e-06 --ff MMFF94s --steps 1000 --rvdw 10 --rele 10 --freq 10 --sd --newton -i mol2 $n -o mol2 -O $pasta/${arquivo%.mol2}.mol2
+  arquivo=$(basename -- $n)
+  obabel -imol -omol --minimize --log --crit 1e-06 --ff MMFF94s --steps 1000 --rvdw 10 --rele 10 --freq 10 --sd --newton -i mol2 $n -o mol2 -O $pasta/${arquivo%.mol2}.mol2
 done
