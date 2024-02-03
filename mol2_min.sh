7307bf884f169a229fc8f12f98ebb3f5c5780057
Leticia Figueira <jlcfigueira@gmail.com>
2024-02-03 10:21:20 -0300

    Adiciona o script que roda o comando do openbabel para fazer a minimização da energia do ligante

---
+++ mol2_min.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pastaOrigem=/content/ZINC\_MOL2
+pasta=/content/ZINC\_MOL2\_min
+mkdir $pasta
+
+for n in $pastaOrigem/*.mol2
+do
+arquivo=$(basename -- $n)
+obabel -imol -omol --minimize --log --crit 1e-06 --ff MMFF94s --steps 1000 --rvdw 10 --rele 10 --freq 10 --sd --newton -i mol2 $n -o mol2 -O $pasta/${arquivo%.mol2}.mol2
+done
